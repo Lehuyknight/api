c388dfdb1a023e8502d13fba34b3293d50acde2d
Lê Huy Knight <loverisaliar@gmail.com>
2022-05-24 09:17:48 +0700

    finalize the UserOrder

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Com9Phut
 API repos cho dịch vụ giao cơm 9 phút
+clone về, npm i để cài package,
+node ace migration:run để add database
+node ace db:seed để install dữ liệu
--- start/routes/user.ts
+++ start/routes/user.ts
@@ -3,5 +3,5 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.group(() =>{
     Route.post('/login','AuthController.login')
     Route.post('/signup','AuthController.signup')
-    Route.post('/signAdmin','AuthController.signupAdmin')
+    Route.post('/signAdmin','AuthController.signupAdmin').middleware('checkAdmin')
 }).prefix('user')
